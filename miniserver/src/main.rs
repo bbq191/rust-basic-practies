700844febc2a4f9334a177f54853295be2d76f7f
Vinci Xu <xsiyuan@gmail.com>
2023-05-24 11:30:18 +0800

    add response

--- miniserver/src/main.rs
+++ miniserver/src/main.rs
@@ -1,4 +1,6 @@
-use std::net::TcpListener;
+use std::fs;
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     // 监听地址: 127.0.0.1:7878
@@ -7,6 +9,23 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let status_line = "HTTP/1.1 200";
+    let contents = fs::read_to_string("src/hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+    // println!("Request: {:#?}", http_request)
 }

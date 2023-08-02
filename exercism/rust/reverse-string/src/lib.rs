5cc7c153e8bb9f215341e14def581316a1a27e75
Vinci Xu <xsiyuan@gmail.com>
2023-08-02 14:33:18 +0800

    add iter2

--- exercism/rust/reverse-string/src/lib.rs
+++ exercism/rust/reverse-string/src/lib.rs
@@ -1,7 +1,9 @@
 pub fn reverse(input: &str) -> String {
-    let mut reverse_string = String::from("");
-    for i in input.chars().rev() {
-        reverse_string.push(i);
-    }
-    reverse_string
+    //let mut reverse_string = String::from("");
+    //for i in input.chars().rev() {
+    //    reverse_string.push(i);
+    //}
+    //reverse_string
+
+    input.chars().rev().collect()
 }

1cdcd29e6fe1eb58a3f664d67fa85209dac810dd
Vinci Xu <xsiyuan@gmail.com>
2023-09-06 19:15:15 +0800

    rm: exercism

--- exercism/rust/reverse-string/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub fn reverse(input: &str) -> String {
-    //let mut reverse_string = String::from("");
-    //for i in input.chars().rev() {
-    //    reverse_string.push(i);
-    //}
-    //reverse_string
-
-    input.chars().rev().collect()
-}
